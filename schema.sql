783f5c9621f7ec60a49e5af80d30fe42650288d6
LeBIIIa <xakep.dead@gmail.com>
2025-06-14 10:07:22 -0400

    Delete schema.sql

--- schema.sql
+++
@@ -1,8 +0,0 @@
-
-CREATE TABLE IF NOT EXISTS applicants (
-    id SERIAL PRIMARY KEY,
-    name TEXT NOT NULL,
-    age INTEGER NOT NULL,
-    city TEXT NOT NULL,
-    telegram_id BIGINT NOT NULL
-);
